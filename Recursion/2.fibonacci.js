e9319c1ff76207666a5f58a547696877a7d9983c
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-08-29 20:35:45 +0400

    changing fibonacci task

--- Recursion/2.fibonacci.js
+++ Recursion/2.fibonacci.js
@@ -1,8 +1,8 @@
 const fibonacci = (num) => {
-  if (num === 1 || num === 2) {
-    return 1;
+  if (num === 0 || num === 1) {
+    return num;
   }
   return fibonacci(num - 1) + fibonacci(num - 2);
 };
-const res = fibonacci(7);
+const res = fibonacci(20);
 console.log(res);
