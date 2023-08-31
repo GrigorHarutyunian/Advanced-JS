0ac71e2fac575a7acec93d9b20afa84e1d38760b
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-08-31 15:43:58 +0400

    binarySearch changing

--- Recursion/6.binarySearch.js
+++ Recursion/6.binarySearch.js
@@ -1,18 +1,24 @@
-const sortedArray = [1, 3, 5, 7, 9, 11, 23, 41];
-const binarySearch = (
-  arr,
-  num,
-  midleElemenet = Math.trunc(arr.length / 2),
-  length = midleElemenet + 1
-) => {
-  if (length === 0) {
+const sortedArray = [1, 3, 5, 7, 11, 23, 42];
+const binarySearch = (arr, num, index = Math.trunc(arr.length / 2)) => {
+  if (
+    arr.length === 0 ||
+    (arr.length === 1 && arr[Math.trunc(arr.length / 2)] !== num)
+  ) {
     return -1;
-  } else if (arr[midleElemenet] === num) {
-    return midleElemenet;
-  } else if (num >= arr[midleElemenet]) {
-    return binarySearch(arr, num, midleElemenet + 1, length - 1);
+  } else if (num === arr[Math.trunc(arr.length / 2)]) {
+    return index;
+  } else if (num >= arr[Math.trunc(arr.length / 2)]) {
+    return binarySearch(
+      arr.slice(Math.trunc(arr.length / 2)),
+      num,
+      index + arr.slice(Math.trunc(arr.length / 2)).length / 2
+    );
   } else {
-    return binarySearch(arr, num, midleElemenet - 1, length - 1);
+    return binarySearch(
+      arr.slice(0, Math.trunc(arr.length / 2)),
+      num,
+      index - Math.round(arr.slice(Math.trunc(arr.length / 2)).length / 2)
+    );
   }
 };
-console.log(binarySearch(sortedArray, 2));
+console.log(binarySearch(sortedArray, 1));
