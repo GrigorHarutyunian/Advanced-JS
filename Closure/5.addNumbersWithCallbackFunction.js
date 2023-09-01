e29c68e3e8960f1e56a8366821a5e67db1e02d4e
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-09-01 16:09:57 +0400

    chang addNumbers task

--- Closure/5.addNumbersWithCallbackFunction.js
+++ Closure/5.addNumbersWithCallbackFunction.js
@@ -1,8 +1,6 @@
-const partial = (callBack, num) => {
-  return (a, b) => callBack(a, b, num);
+const partial = (add, num3) => {
+  return (...nums) => add(...nums, num3);
 };
-const add = (a, b, c) => a + b + c;
-
-const add5 = partial(add, 5);
-console.log(add5(10, 20));
-console.log(add5(101, 211));
+const add = (...nums) => nums.reduce((acc, val) => acc + val);
+const add5 = partial(add, 4);
+console.log(add5(1, 3, 5));
