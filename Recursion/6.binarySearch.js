76de356fd890fa53115028e6fe210acf983d9c2e
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-09-01 16:40:28 +0400

    change in task

--- Recursion/6.binarySearch.js
+++ Recursion/6.binarySearch.js
@@ -1,24 +1,22 @@
 const sortedArray = [1, 3, 5, 7, 11, 23, 42];
 const binarySearch = (arr, num, index = Math.trunc(arr.length / 2)) => {
-  if (
-    arr.length === 0 ||
-    (arr.length === 1 && arr[Math.trunc(arr.length / 2)] !== num)
-  ) {
+  const midleElem = Math.trunc(arr.length / 2);
+  if (arr.length === 0 || (arr.length === 1 && arr[midleElem] !== num)) {
     return -1;
-  } else if (num === arr[Math.trunc(arr.length / 2)]) {
+  } else if (num === arr[midleElem]) {
     return index;
-  } else if (num >= arr[Math.trunc(arr.length / 2)]) {
+  } else if (num >= arr[midleElem]) {
     return binarySearch(
-      arr.slice(Math.trunc(arr.length / 2)),
+      arr.slice(midleElem),
       num,
-      index + arr.slice(Math.trunc(arr.length / 2)).length / 2
+      index + arr.slice(midleElem).length / 2
     );
   } else {
     return binarySearch(
-      arr.slice(0, Math.trunc(arr.length / 2)),
+      arr.slice(0, midleElem),
       num,
-      index - Math.round(arr.slice(Math.trunc(arr.length / 2)).length / 2)
+      index - Math.round(arr.slice(midleElem).length / 2)
     );
   }
 };
-console.log(binarySearch(sortedArray, 1));
+console.log(binarySearch(sortedArray, 42));
