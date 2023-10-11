754dd24102a57a3fd7c447f19630ef1a0cc49ede
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-10-12 00:21:03 +0400

    changing setRemoveDuplicates

--- Data Structures/7.setRemoveDuplicates.js
+++ Data Structures/7.setRemoveDuplicates.js
@@ -1,4 +1,4 @@
 const arr = [1, 2, 3, 4, 1, 1, 3, 2, 4, 1, 13, 4, 2, 1, 4];
-const removeDuplicates = (array) => [...new Set([...array])];
+const removeDuplicates = (array) => [...new Set(array)];
 const res = removeDuplicates(arr);
 console.log(res);
